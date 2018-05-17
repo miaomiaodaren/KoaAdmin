735965c9e35ed771f3948eee8428709f180847d0
chuanshanjia <chanshanjia@lljz-inc.com>
2018-05-18 00:35:16 +0800

    add blog add and blog del

--- controllers/blog.ts
+++ controllers/blog.ts
@@ -13,4 +13,48 @@ export default class BlogController {
             handleError({ctx, err})
         }    
     }
+
+    public static async AddBlog(ctx, next) {
+        const method = ctx.method, param = method === 'GET' ? ctx.query : ctx.request.body;
+        const {title, type, content, _id} = param; const addtime = new Date();
+        if(!title) handleError({ctx, err: '标题不能为空'})
+        if(!type) handleError({ctx, err: '类别不能为空'})
+        if(!content) handleError({ctx, err: '内容不能为空'})
+		if(_id) {
+            try{
+                const info = await BlogModel.update({'_id': _id}, {$set: {'title': title, 'type': type, 'content': content, 'addtime': addtime}});
+                handleSuccess({ctx, message: '添加新闻成功!'})
+            } catch(err) {
+                if(!content) handleError({ctx, err: '添加新闻失败'})
+            }
+		} else {
+			const addnew = new BlogModel({
+				title: title,
+				type: type,
+				content: content,
+				addtime: addtime
+			});
+			try {
+				await addnew.save();
+                handleSuccess({ctx, message: '新闻发布成功!'})
+			} catch(err) {
+                handleError({ctx, err: '添加新闻失败'})
+			};
+		}
+    }
+
+    public static async DelBlog(ctx, next) {
+        const method = ctx.method, param = method === 'GET' ? ctx.query : ctx.request.body;
+        const { _id } = param;
+        if(_id) {
+            try{
+                const info = await BlogModel.remove({'_id': _id});
+                if(info) handleSuccess({ctx, message: '消息删除成功!'})
+            } catch(err) {
+                handleSuccess({ctx, message: '消息删除失败!'})
+            }
+        } else {
+            handleSuccess({ctx, message: '缺少必要参数!'})
+        }
+    }
 }
--- router/blog.ts
+++ router/blog.ts
@@ -3,6 +3,7 @@ const router = new Router();
 import { Blog } from '../controllers/index';
 
 router.get('/blogList', Blog.GetBlogList);
-
+router.post('/addBlog', Blog.AddBlog);
+router.post('/delBlog', Blog.DelBlog);
 
 export default router
